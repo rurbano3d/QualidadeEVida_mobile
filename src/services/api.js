c0c67e3213cd3bf957ba483684411a3e7efc14ad
Rodrigo Urbano <ururbano@rurbano3d.local>
2020-06-10 18:57:41 -0300

    production

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const api = axios.create({
-  baseURL:
-    Platform.OS === 'ios'
-      ? 'http://localhost:3333'
-      : 'http://192.168.0.104:3333',
-  // baseURL: 'https://apiqualidadevida.ddns.net',
+  // baseURL:
+  //   Platform.OS === 'ios'
+  //     ? 'http://localhost:3333'
+  //     : 'http://192.168.0.104:3333',
+  baseURL: 'https://apiqualidadevida.ddns.net',
 });
 
 export default api;
